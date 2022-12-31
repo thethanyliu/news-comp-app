1646ff70d6dc5fa868f4354e3431343bc7a1bbea
Ethan <62630748+ethanliu666@users.noreply.github.com>
2022-12-30 23:47:05 -0500

    updated css

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        burtons: "burton",
+      },
+    },
+  },
+  plugins: [],
+}
